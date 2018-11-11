fe85c6ad815e182b17c15101cd6f14a461e90548
thirtylynx <thirtylynx@gmail.com>
2018-11-11 22:58:21 +0500

    Создал тестовые model view collection, добавил шаблон

--- src/js/index.js
+++ src/js/index.js
@@ -9,4 +9,45 @@ var Person = Backbone.Model.extend({
 	}
 });
 
+var PeopleCollection = Backbone.Collection.extend({
+	model: Person
+});
+
+var PersonView = Backbone.View.extend({
+	initialize: function() {
+		console.log('initialize!')
+  },
+  tagName: 'li',
+
+	template: _.template( $('#person-id').html() ),
+  
+	initialize: function() {
+		this.render();
+	},
+   
+  render: function() {
+    this.$el.html( this.template(this.model.toJSON()) );
+  }
+});
+
 var person = new Person;
+var personView = new PersonView({ model: person });
+
+var people = [
+	{
+		name: 'Петр',
+		age: 20,
+		job: 'Таксист'
+	},
+	{
+		name: 'Олег',
+		job: 'Менеджер'
+	},
+	{
+		name: 'Анна',
+		age: 18,
+		job: 'Студентка'
+	}
+]
+var peopleCollection = new PeopleCollection(people)
+
